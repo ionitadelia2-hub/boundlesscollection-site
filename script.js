f6d27f98395242c00ac641a941f9acb15a511f4d
ionitadelia22 <ionitadelia2@gmail.com>
2025-10-16 10:06:28 +0300

    .

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function render(){
 /* =============== Loader JSON =============== */
 async function loadProducts(){
   try{
-    const res = await fetch('./products.json?cachebust=' + Date.now(), {
+    const res = await fetch('./content/products.json?cachebust=' + Date.now(), {
       headers: { 'Accept': 'application/json' }
     });
     if(!res.ok) throw new Error('Nu s-a putut încărca products.json');
